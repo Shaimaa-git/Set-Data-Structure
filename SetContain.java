cc03b2e630b293f562248d654ac6767e45721b02
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-26 12:04:12 +0400

    check if name John is found on the set

---
+++ SetContain.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+public class Main {
+    public static void main(String[] args) {
+         Set <String> myset=new HashSet<>();
+         myset.add("Shaima");
+         myset.add("Salim");
+         myset.add("John");
+         myset.add("Afnan");
+         myset.add("Sulaiman");
+         //print the set
+        System.out.println(myset);
+        
+        //check if name John is found
+        if(myset.contains("John"))
+        {
+            System.out.println("Name John is found");
+        }
+        else{
+            System.out.println("Name John is not found");
+        }
+    }
+}
